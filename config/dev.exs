4aa565a8577b62f6d38596aaa3974415236b107c
Elfred Pagan <epagan@twitter.com>
2017-02-15 15:59:17 -0600

    remove feedback host/port

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,6 @@ use Mix.Config
 # config :apns, :config,
 #   push_host: "gateway.sandbox.push.apple.com",
 #   push_port: 2195,
-#   feedback_host: "feedback.sandbox.push.apple.com",
-#   feedback_port: 2196,
 #   cert: "priv/cert.pem",
 #   key: "priv/key.pem",
 #   feedback_handler: APNS.FeedbackHandler
